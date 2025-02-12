ce2c1603d4e36eb04e15c646a059b38c6108c6fb
Christian Umoh <umohchristian001@gmail.com>
2025-02-12 16:16:59 +0100

    testing

--- main.py
+++ main.py
@@ -21,4 +21,3 @@ app.include_router(api_router, prefix=settings.API_PREFIX)
 async def health_check():
     """Checks if server is active."""
     return {"status": "active"}
-print("checking out the test branch")
